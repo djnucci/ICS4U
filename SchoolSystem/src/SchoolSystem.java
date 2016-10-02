42fcaba85c3653f4ab35d16125ef1a8f49e2bbb2
Daniel Nucci <djnucci@hotmail.com>
2016-10-01 23:06:53 -0400

    Finished the varifying methods for the Student Class and deleting a single student from an index

--- SchoolSystem/src/SchoolSystem.java
+++ SchoolSystem/src/SchoolSystem.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class SchoolSystem {
@@ -7,6 +6,7 @@ public class SchoolSystem {
 	public static ArrayList<Student> listOfStudents = new ArrayList<Student>();
 
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		int directoryIndex = 0;
 		int studentNumber = 0;
@@ -23,7 +23,8 @@ public class SchoolSystem {
 			}
 			if (directoryIndex == 1) {
 				addStudent();
-			} else if (directoryIndex == 2) {
+			}
+			else if (directoryIndex == 2) {
 				System.out.println("What student do you want to print? (number)");
 				while (true) {
 					try {
@@ -34,44 +35,67 @@ public class SchoolSystem {
 					}
 
 				}
-				printStudent(studentNumber);
+				if (listOfStudents.size() > studentNumber && studentNumber != -1) {
+					printStudent(studentNumber);
+				}
+				else {
+					System.out.println("Invalid");
+				}
 			}
-			else if(directoryIndex == 3){
+			else if (directoryIndex == 3) {
 				listOfStudents.trimToSize();
-				
-				for (int i = 0; i < listOfStudents.size(); i++){
+
+				for (int i = 0; i < listOfStudents.size(); i++) {
 					printStudent(i);
-					System.out.println("_______________________________");
+					System.out.println("___________________________________");
 				}
 			}
-			else if(directoryIndex == 4){
-				System.out.println("WIP");
+			else if (directoryIndex == 4) {
+				System.out.println("What student do you want to delete? (number)");
+				while (true) {
+					try {
+						studentNumber = Integer.parseInt(scan.nextLine()) - 1;
+						break;
+					} catch (NumberFormatException ie) {
+						System.out.println("Try an int next time");
+					}
+				}
+				if (listOfStudents.size() > studentNumber && studentNumber != -1) {
+					printStudent(studentNumber);
+					System.out.println("Are you sure you want to delete?");
+					if (scan.nextLine().equalsIgnoreCase("Yes")) {
+						deleteStudent(studentNumber);
+					}
+				}
+				else {
+					System.out.println("Invalid");
+				}
 			}
-			else if(directoryIndex == 5){
+			else if (directoryIndex == 5) {
 				System.out.println("NOT IMPLEMENTED YET");
 			}
-			else if(directoryIndex == 6){
+			else if (directoryIndex == 6) {
 				System.out.println("NOT IMPLEMENTED YET");
 			}
-			else if(directoryIndex == 7){
+			else if (directoryIndex == 7) {
 				System.out.println("NOT IMPLEMENTED YET");
 			}
-			else if(directoryIndex == 8){
+			else if (directoryIndex == 8) {
 				System.out.println("NOT IMPLEMENTED YET");
 			}
-			else if(directoryIndex == 9){
+			else if (directoryIndex == 9) {
 				System.out.println("NOT IMPLEMENTED YET");
 			}
-			else if(directoryIndex == 10){
+			else if (directoryIndex == 10) {
 				System.out.println("Are you sure?");
-				if (scan.nextLine().equalsIgnoreCase("Yes")){
-				break;
+				if (scan.nextLine().equalsIgnoreCase("Yes")) {
+					break;
 				}
 			}
-			else{
+			else {
 				System.out.println("Please enter a correct value.");
 			}
-			
+
 		}
 
 	}
@@ -80,7 +104,10 @@ public class SchoolSystem {
 	 * Appends a student to the global array
 	 */
 	public static void addStudent() {
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
+
+		String userInput = "";
 
 		listOfStudents.trimToSize();
 		listOfStudents.add(new Student());
@@ -92,7 +119,15 @@ public class SchoolSystem {
 		listOfStudents.get(listOfStudents.size() - 1).setLastName(scan.nextLine());
 
 		System.out.println("Please enter your birth date (mm/dd/yyyy): ");
-		listOfStudents.get(listOfStudents.size() - 1).setBirthDate(scan.nextLine());
+		userInput = scan.nextLine();
+		if (listOfStudents.get(listOfStudents.size() - 1).varifyBirthDate(userInput)) {
+			listOfStudents.get(listOfStudents.size() - 1).setBirthDate(userInput);
+		}
+		else {
+			listOfStudents.remove(listOfStudents.size() - 1);
+			System.out.println("Invalid input, please try again.");
+			return;
+		}
 
 		System.out.println("Please enter the city you live in: ");
 		listOfStudents.get(listOfStudents.size() - 1).setCity(scan.nextLine());
@@ -101,19 +136,42 @@ public class SchoolSystem {
 		listOfStudents.get(listOfStudents.size() - 1).setProvince(scan.nextLine());
 
 		System.out.println("Please enter your current street address: ");
-		listOfStudents.get(listOfStudents.size() - 1).setStreetAddress(scan.nextLine());
+		userInput = scan.nextLine();
+		if (listOfStudents.get(listOfStudents.size() - 1).varifyStreetAddress(userInput)) {
+			listOfStudents.get(listOfStudents.size() - 1).setStreetAddress(userInput);
+		}
+		else {
+			listOfStudents.remove(listOfStudents.size() - 1);
+			System.out.println("Invalid input, please try again.");
+			return;
+		}
 
-		System.out.println("Please enter your postal code (X0X 0X0): ");
-		listOfStudents.get(listOfStudents.size() - 1).setPostalCode(scan.nextLine());
-
-		System.out.println("Please enter your phone number ((xxx) xxx - xxxx):");
-		listOfStudents.get(listOfStudents.size() - 1).setPhoneNumber(scan.nextLine());
+		System.out.println("Please enter your postal code (X1X 1X1): ");
+		userInput = scan.nextLine();
+		if (listOfStudents.get(listOfStudents.size() - 1).varifyPostalCode(userInput)) {
+			listOfStudents.get(listOfStudents.size() - 1).setPostalCode(userInput);
+		}
+		else {
+			listOfStudents.remove(listOfStudents.size() - 1);
+			System.out.println("Invalid input, please try again.");
+			return;
+		}
+		System.out.println("Please enter your phone number ((xxx) - xxx - xxxx):");
+		userInput = scan.nextLine();
+		if (listOfStudents.get(listOfStudents.size() - 1).varifyPhoneNumber(userInput)) {
+			listOfStudents.get(listOfStudents.size() - 1).setPhoneNumber(userInput);
+		}
+		else {
+			listOfStudents.remove(listOfStudents.size() - 1);
+			System.out.println("Invalid input, please try again.");
+			return;
+		}
 	}
 
 	/**
 	 * Prints the introduction to the user
 	 */
-	public static void printIntro() {
+	private static void printIntro() {
 		System.out.println("School System Database Directions".toUpperCase());
 		System.out.println("\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/");
 		System.out.println("Type  ( 1 ) to enter a new student.");
@@ -128,13 +186,17 @@ public class SchoolSystem {
 		System.out.println("Type ( 10 ) to quit program");
 	}
 
+	public static void deleteStudent(int index) {
+		listOfStudents.remove(index);
+	}
+
 	/**
 	 * This prints the student at an index
 	 * 
 	 * @param i
-	 * 		int - index of arraylist
+	 *           int - index of arraylist
 	 */
-	public static void printStudent(int i) {
+	private static void printStudent(int i) {
 		System.out.println(listOfStudents.get(i).getFirstName());
 		System.out.println(listOfStudents.get(i).getLastName());
 		System.out.println(listOfStudents.get(i).getBirthDate());
@@ -144,72 +206,79 @@ public class SchoolSystem {
 		System.out.println(listOfStudents.get(i).getPostalCode());
 		System.out.println(listOfStudents.get(i).getPhoneNumber());
 	}
-	
+
 	/**
 	 * This method calls the corresponding method to the number enter by the user
 	 * 
 	 * @param list
-	 * 		ArrayList - the list of students
+	 *           ArrayList - the list of students
 	 * @param identity
-	 * 		int - the sort method you want to call e.g. (1 = first name, 2 = last name, 3 = street address, 4 = city, 5 = provincial , 6 = postal code, 7 = phone number, 8 = birthdate)
+	 *           int - the sort method you want to call e.g. (1 = first name, 2 = last name, 3 = street address, 4 = city, 5 = provincial , 6 = postal code, 7 = phone number, 8 = birthdate)
 	 */
-	public static void sortArrayList(ArrayList<?> list, int identity){
-		if (identity == 1){
+	private static void sortArrayList(ArrayList<?> list, int identity) {
+		if (identity == 1) {
 			sortByFirstName(list);
 		}
-		else if (identity == 2){
+		else if (identity == 2) {
 			sortByLastName(list);
 		}
-		else if (identity == 3){
+		else if (identity == 3) {
 			sortByStreetAddress(list);
 		}
-		else if (identity == 4){
+		else if (identity == 4) {
 			sortByCity(list);
 		}
-		else if (identity == 5){
+		else if (identity == 5) {
 			sortByProvince(list);
 		}
-		else if (identity == 6){
+		else if (identity == 6) {
 			sortByPostalCode(list);
 		}
-		else if (identity == 7){
+		else if (identity == 7) {
 			sortByPhoneNumber(list);
 		}
-		else if (identity == 8){
+		else if (identity == 8) {
 			sortByBirthDate(list);
 		}
 		else {
 			return;
 		}
 	}
-	
-	public static void sortByFirstName(ArrayList<?> list){
-		
+
+	private static void sortByFirstName(ArrayList<?> list) {
+
 	}
-	public static void sortByLastName(ArrayList<?> list){
-		
+	private static void sortByLastName(ArrayList<?> list) {
+
 	}
-	public static void sortByStreetAddress(ArrayList<?> list){
-		
+	private static void sortByStreetAddress(ArrayList<?> list) {
+
 	}
-	public static void sortByCity(ArrayList<?> list){
-		
+	private static void sortByCity(ArrayList<?> list) {
+
 	}
-	public static void sortByProvince(ArrayList<?> list){
-		
+	private static void sortByProvince(ArrayList<?> list) {
+
 	}
-	public static void sortByPostalCode(ArrayList<?> list){
-		
+	private static void sortByPostalCode(ArrayList<?> list) {
+
 	}
-	public static void sortByPhoneNumber(ArrayList<?> list){
-		
+	private static void sortByPhoneNumber(ArrayList<?> list) {
+
 	}
-	public static void sortByBirthDate(ArrayList<?> list){
-		
+	private static void sortByBirthDate(ArrayList<?> list) {
+
 	}
-	
-	public static int binarySearch(ArrayList<?> list){
-	
+	/**
+	 * 
+	 * @param keyWord
+	 * @param list
+	 * @param sortMethod
+	 * @return
+	 */
+	private static int binarySearch(String keyWord, ArrayList<Student> studentID, int sortMethod) {
+		sortArrayList(studentID, sortMethod);
+
 		return -1;
 	}
 
--- SchoolSystem/src/Student.java
+++ SchoolSystem/src/Student.java
@@ -1,14 +1,13 @@
+import java.util.ArrayList;
 import java.util.regex.PatternSyntaxException;
 
 public class Student {
 	private String firstName, lastName, streetAddress, city, province, postalCode, phoneNumber, birthDate;
 
-	
 	/**
 	 * Gets the student's birth date
 	 * 
-	 * @return
-	 * 		String - his/her birth date
+	 * @return String - his/her birth date
 	 */
 	public String getBirthDate() {
 		return birthDate;
@@ -17,42 +16,65 @@ public class Student {
 	/**
 	 * Sets the student's birth date
 	 * 
-	 * @param birthDate	
-	 * 		String - his/her birth date
+	 * @param birthDate
+	 *           String - his/her birth date
 	 */
 	public void setBirthDate(String birthDate) {
 		this.birthDate = birthDate;
 	}
-	
+
 	/**
 	 * Makes sure the user inputed student's birth date is the correct formatting
 	 * 
-	 * @param birthDate	
-	 * 		String - his/her potential birth date
+	 * @param birthDate
+	 *           String - his/her potential birth date
+	 * @return boolean - returns if valid
 	 */
 	public boolean varifyBirthDate(String birthDate) {
+		int[] monthLengths = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 		String[] splitText;
-		int streetNumber = 0;
-		
-		try{
-			splitText = birthDate.split(" ");
-		}catch (PatternSyntaxException ie){
-			return false;			
-		}
-		try{
-			streetNumber = Integer.parseInt(splitText[0]);
-		}catch (NumberFormatException ie){
+		try {
+			splitText = birthDate.split("/");
+		} catch (PatternSyntaxException ie) {
 			return false;
 		}
-		
+		int day, month, year;
+
+		if (splitText.length != 3) {
+			return false;
+		}
+
+		try {
+			day = Integer.parseInt(splitText[1]);
+			month = Integer.parseInt(splitText[0]);
+			year = Integer.parseInt(splitText[2]);
+		} catch (NumberFormatException ie) {
+			return false;
+		}
+
+		if (year % 4 == 0 && year % 100 != 0 && year % 400 == 0) {
+			monthLengths[1] = 29;
+		}
+
+		if ((2016 - year) < 13 || (2016 - year) > 18) {
+			return false;
+		}
+
+		if (month > 12 || month <= 0) {
+			return false;
+		}
+
+		if (monthLengths[month - 1] < day) {
+			return false;
+		}
+
 		return true;
 	}
 
 	/**
 	 * Gets the student's city
 	 * 
-	 * @return
-	 * 		String - his/her city
+	 * @return String - his/her city
 	 */
 	public String getCity() {
 		return city;
@@ -62,7 +84,7 @@ public class Student {
 	 * Sets the student's
 	 * 
 	 * @param city
-	 * 		String - his/her city
+	 *           String - his/her city
 	 */
 	public void setCity(String city) {
 		this.city = city;
@@ -71,8 +93,7 @@ public class Student {
 	/**
 	 * Gets the student's first name
 	 * 
-	 * @return
-	 * 		String - his/her first name
+	 * @return String - his/her first name
 	 */
 	public String getFirstName() {
 		return firstName;
@@ -82,7 +103,7 @@ public class Student {
 	 * Sets the student's first name
 	 * 
 	 * @param firstName
-	 * 		String - his/her first name
+	 *           String - his/her first name
 	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
@@ -91,8 +112,7 @@ public class Student {
 	/**
 	 * Gets the student's last name
 	 * 
-	 * @return
-	 * 		String - his/her last name
+	 * @return String - his/her last name
 	 */
 	public String getLastName() {
 		return lastName;
@@ -102,7 +122,7 @@ public class Student {
 	 * Sets the student's last name
 	 * 
 	 * @param lastName
-	 * 		String - his/her last name
+	 *           String - his/her last name
 	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
@@ -111,8 +131,7 @@ public class Student {
 	/**
 	 * Gets the student's phone number
 	 * 
-	 * @return
-	 * 		String - his/her phone number
+	 * @return String - his/her phone number
 	 */
 	public String getPhoneNumber() {
 		return phoneNumber;
@@ -122,17 +141,64 @@ public class Student {
 	 * Sets the student's phone number
 	 * 
 	 * @param phoneNumber
-	 * 		String - his/her phone number
+	 *           String - his/her phone number
 	 */
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
 	/**
+	 * Makes sure the user inputed student's phone number is the correct formatting
+	 * 
+	 * @param phoneNumber
+	 *           String - his/her potential phone number
+	 * @return boolean - returns if valid
+	 */
+	public boolean varifyPhoneNumber(String phoneNumber) {
+		String[] splitText;
+		try {
+			splitText = phoneNumber.split(" - ");
+		} catch (PatternSyntaxException ie) {
+			return false;
+		}
+
+		if (splitText.length != 3) {
+			return false;
+		}
+
+		char[] firstPart = splitText[0].toCharArray();
+		int secondPart, thirdPart;
+
+		if (firstPart.length != 5) {
+			return false;
+		}
+
+		if (!equalsNumber(firstPart[1]) || !equalsNumber(firstPart[2]) || !equalsNumber(firstPart[3])) {
+			return false;
+		}
+
+		try {
+			secondPart = Integer.parseInt(splitText[1]);
+			thirdPart = Integer.parseInt(splitText[1]);
+		} catch (NumberFormatException ie) {
+			return false;
+		}
+
+		if (secondPart < 0 || secondPart > 999) {
+			return false;
+		}
+
+		if (thirdPart < 0 || thirdPart > 9999) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
 	 * Gets the student's postal code
 	 * 
-	 * @return
-	 * 		String - his/her postal code
+	 * @return String - his/her postal code
 	 */
 	public String getPostalCode() {
 		return postalCode;
@@ -142,17 +208,71 @@ public class Student {
 	 * Sets the student's postal code
 	 * 
 	 * @param postalCode
-	 * 		String - his/her postal code
+	 *           String - his/her postal code
 	 */
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
 
 	/**
+	 * Makes sure the user inputed student's postal code is the correct formatting
+	 * 
+	 * @param postalCode
+	 *           String - his/her potential postal code
+	 * @return boolean - returns if valid
+	 */
+	public boolean varifyPostalCode(String postalCode) {
+		String[] splitText = new String[2];
+		try {
+			splitText = postalCode.split(" ");
+		} catch (PatternSyntaxException ie) {
+			return false;
+		}
+		char[] sideOne;
+		char[] sideTwo;
+		try {
+			sideOne = splitText[0].toCharArray();
+			sideTwo = splitText[1].toCharArray();
+		} catch (ArrayIndexOutOfBoundsException ie) {
+			return false;
+		}
+		if (sideOne.length != 3 || sideTwo.length != 3) {
+			return false;
+		}
+
+		for (int i = 1; i < (sideOne.length - 1); i++) {
+			if (i % 2 == 1) {
+				if (!equalsUpperCaseLetter(sideOne[i - 1])) {
+					return false;
+				}
+			}
+			else {
+				if (!equalsNumber(sideOne[i - 1])) {
+					return false;
+				}
+			}
+		}
+
+		for (int i = 1; i < (sideTwo.length - 1); i++) {
+			if (i % 2 == 0) {
+				if (!equalsUpperCaseLetter(sideTwo[i - 1])) {
+					return false;
+				}
+			}
+			else {
+				if (!equalsNumber(sideTwo[i - 1])) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	/**
 	 * Gets the student's province
 	 * 
-	 * @return
-	 * 		String - his/her province
+	 * @return String - his/her province
 	 */
 	public String getProvince() {
 		return province;
@@ -162,7 +282,7 @@ public class Student {
 	 * Sets the student's province
 	 * 
 	 * @param province
-	 * 		String - his/her province
+	 *           String - his/her province
 	 */
 	public void setProvince(String province) {
 		this.province = province;
@@ -171,8 +291,7 @@ public class Student {
 	/**
 	 * Gets the student's street address
 	 * 
-	 * @return
-	 * 		String - his/her street address
+	 * @return String - his/her street address
 	 */
 	public String getStreetAddress() {
 		return streetAddress;
@@ -182,10 +301,67 @@ public class Student {
 	 * Sets the student's street address
 	 * 
 	 * @param streetAddress
-	 * 		String - his/her street address
+	 *           String - his/her street address
 	 */
 	public void setStreetAddress(String streetAddress) {
 		this.streetAddress = streetAddress;
 	}
-	
+
+	/**
+	 * Makes sure the user inputed student's street address is the correct formatting
+	 * 
+	 * @param streetAddress
+	 *           String - his/her potential street address
+	 * @return boolean - reurns if valid
+	 */
+	public boolean varifyStreetAddress(String streetAddress) {
+		String[] splitText;
+		try {
+			splitText = streetAddress.split(" ");
+		} catch (PatternSyntaxException ie) {
+			return false;
+		}
+		if (splitText.length < 2) {
+			return false;
+		}
+		try {
+			Integer.parseInt(splitText[0]);
+		} catch (NumberFormatException ie) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * This method check to see if a char equals a letter
+	 * 
+	 * @param letter
+	 *           char - the char in question
+	 * @return boolean - true if a letter, false if not
+	 */
+	public static boolean equalsUpperCaseLetter(char letter) {
+		if (letter == 'A' || letter == 'B' || letter == 'C' || letter == 'D' || letter == 'E' || letter == 'F' || letter == 'G' || letter == 'H' || letter == 'I' || letter == 'J' || letter == 'K' || letter == 'L' || letter == 'M' || letter == 'N' || letter == 'O' || letter == 'P' || letter == 'Q' || letter == 'R' || letter == 'S' || letter == 'T' || letter == 'U' || letter == 'V' || letter == 'W' || letter == 'X' || letter == 'Y' || letter == 'Z') {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	/**
+	 * This method check to see if a char equals a number above zero
+	 * 
+	 * @param number
+	 *           char - the char in question
+	 * @return boolean - true if a letter, false if not
+	 */
+	public static boolean equalsNumber(char number) {
+		if (number == '1' || number == '2' || number == '3' || number == '4' || number == '5' || number == '6' || number == '7' || number == '8' || number == '9') {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
 }
